b2724970a53a90228f0161686c4a35d455753150
Goldent00thbrush <44409654+Goldent00thbrush@users.noreply.github.com>
2019-11-17 17:53:27 +0200

    Add files via upload

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,4 +1,5 @@
 #include "../headers/Client.h"
+#define PATH "/mnt/d/college/Semester 12-- Fall 2019/CSCE 4411 - Fund of Dist Sys/project/Distributed-Client-master (16)/Distributed-Client-master/"
 #include <iostream>
 using namespace std ;
 
@@ -12,10 +13,12 @@ enum serviceOperations{
 
 
 Client::Client(const char * listen_hostname, const char * listen_port){
+    //std::string path = (std::string)PATH+"client_db.txt";
     buffer = new char [buff_size];
     this->port = listen_port ;
     this->hostname = listen_hostname ;
     std::string serverIp = hostname_to_ip((char *)listen_hostname);
+    //db = new Database(path);
     reqReply = new RequestReply(listen_port,  serverIp.c_str(), true, 1024);
 }
 Message buildRequestMsg(serviceOperations operation, int image_id ){
@@ -28,13 +31,14 @@ Message buildRequestMsg(serviceOperations operation, int image_id ){
     Message msg = Message(reqinfo);
     return msg;
 }
-void saveImage(std::string image, int image_id){
-    std::string temp_loc = "/Users/owner/CLionProjects/Distributed-Client/images/requested/" + to_string(image_id) + ".jpg";
+std::string saveImage(std::string image, int image_id){
+    std::string temp_loc = (std::string)PATH+"images/requested/" + to_string(image_id) + ".jpg";
     std::ofstream outFile;
     outFile.open(temp_loc);
     outFile << image;
     outFile.close();
     std::string secret_text = stega_decode(temp_loc);
+    return secret_text;
 }
 void Client::executePrompt() {
     cout << "Enter Request Number" ;
@@ -85,10 +89,55 @@ void Client::executePrompt() {
                 Message reply_msg = Message();
                 if(reqReply->getReply(reply_msg) >= 0){
                     saveImage(reply_msg.getMessage(), reply_msg.getImageId());
+
+                    /*std::vector<std::string> result;
+                    std::istringstream iss(s);
+                    for(std::string s; iss >> s; )
+                        result.push_back(s);
+                    /*for (std::vector<std::string>::const_iterator i = result.begin(); i != result.end(); ++i)
+                        std::cout << *i << endl;
+                    int r = std::atoi(result[1].c_str());
+                    std::tuple<std::string, int> t((""+result[3]+result[4]),r);
+                    //db->insertUser(image_id,t);
+                    */
                 }
             }
             break ;
         }
+    case 3: //update views when viewing image
+    {
+
+        int image_id;
+        cout << "Which Picture would you like to view ? ";
+        cin >> image_id;
+
+        std::string path = (std::string)PATH+"images/requested/" + to_string(image_id)+ ".jpg";
+        std::string temp_path = (std::string)PATH+"images/stego/" + to_string(image_id)+ ".jpg";
+        std::string s = stega_decode(path);
+        std::cout<<s<<endl;
+
+        std::vector<std::string> result; //parsing string
+        std::istringstream iss(s);
+        for(std::string s; iss >> s; )
+            result.push_back(s);
+         if (result[1]== "0")
+             printf("error cannot access photo");
+         else
+            {     result[1] =to_string(atoi(result[1].c_str()) -1);
+         std::string new_s = "";
+         for (std::vector<std::string>::const_iterator i = result.begin(); i != result.end(); ++i)
+                                 new_s += ((std::string)*i + " ");
+         std::cout<<new_s<<endl;
+         std::string stego_image = stega_encode(path, new_s, temp_path);
+         saveImage(stego_image, image_id);
+
+        //send to server
+        Message msg = buildRequestMsg(DecrementView,image_id);
+        msg.setMessage (result[1], result[1].length());
+        int req_status = reqReply->sendReq(msg);
+         }
+        break ;
+    }
     }
 }
 int Client::requestNumber(int req) {
@@ -112,19 +161,6 @@ int Client::requestSamples(std::string s ) {
     //reqReply->shutDownFD();
 }
 
-bool Client::req_photo(int photoid, const char *hostname, const char *port)
-{
-    int requestStatus, replyStatus;
-    List::entry e;
-    e.port= (char*)port;
-    e.hostname=(char*)hostname;
-    e.photoid=photoid;
-    int val = 0;
-    if(!lookup_table.search(e))  //define search function as well the undefined crap if needed
-    {
-        lookup_table.insertFirst(e,val);  //could be a table of pic/needs to expand
-    }
-}
 
 Client::~Client(){
      reqReply->shutDownFD();
